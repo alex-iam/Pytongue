96052b57bc6b1a99ea41d0baf410477912a2364a
alex-korzh <alex@korzh.me>
2024-10-20 10:23:12 +0200

    handlers lines

--- src/handlers.zig
+++ src/handlers.zig
@@ -1,27 +1,19 @@
 const Server = @import("server.zig").Server;
 const std = @import("std");
 
-pub fn handleInitialize(
-    _: []const u8,
-) []const u8 {
+pub fn handleInitialize(_: []const u8) []const u8 {
     std.log.debug("handleInitialize", .{});
     return "Initialized with basic capabilities\n";
 }
-pub fn handleExit(
-    _: []const u8,
-) []const u8 {
+pub fn handleExit(_: []const u8) []const u8 {
     std.log.debug("handleExit\n", .{});
     return "";
 }
-pub fn handleShutown(
-    _: []const u8,
-) []const u8 {
+pub fn handleShutown(_: []const u8) []const u8 {
     std.log.debug("shutdown request", .{});
     return "Server shutting down\n";
 }
-pub fn handleUnknown(
-    _: []const u8,
-) []const u8 {
+pub fn handleUnknown(_: []const u8) []const u8 {
     std.log.debug("unknown request", .{});
     return "Unknown method\n";
 }
