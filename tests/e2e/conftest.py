1f0808ed6b2a40e4df89a450c911109e82966c7f
alex-korzh <alex@korzh.me>
2024-10-23 14:28:30 +0200

    refactor tests

---
+++ tests/e2e/conftest.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+import time
+
+import pytest
+
+
+@pytest.fixture
+def binary():
+    return os.getenv("PYTONGUE_TEST_BINARY", "./zig-out/bin/pytongue")
+
+
+@pytest.fixture
+def version():
+    with open("version") as f:
+        return f.read().strip()
+
+
+@pytest.fixture
+def simple_initialize_request():
+    return {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": None,
+    }
+
+
+@pytest.fixture
+def server(binary):
+    server = subprocess.Popen(
+        [binary],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=os.environ.copy(),
+    )
+    yield server
+
+    server.terminate()
+    server.wait()
+
+
+@pytest.fixture
+def send_request(server):
+    def _send_request(request):
+        content_length = len(request)
+        server.stdin.write(
+            f"Content-Length: {content_length}\r\n\r\n{request}".encode()
+        )
+        server.stdin.flush()
+
+    return _send_request
+
+
+@pytest.fixture
+def send_arbitrary_request(server):
+    def _send_arbitrary_request(request):
+        server.stdin.write(request.encode())
+        server.stdin.flush()
+
+    return _send_arbitrary_request
+
+
+@pytest.fixture
+def read_response(server):
+    def _read_response():
+        header = server.stdout.readline().decode().strip()
+        content_length = int(header.split(": ")[1])
+        server.stdout.readline()  # Empty line
+        return server.stdout.read(content_length).decode()
+
+    return _read_response
--- tests/e2e/test_lifecycle.py
+++ tests/e2e/test_lifecycle.py
@@ -1,70 +1,27 @@
-import os
-import subprocess
-import time
 import json
 
 
-class TestServerLifecycle:
-    @classmethod
-    def setup_class(cls):
-        cls.request_data = {
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": "initialize",
-            "params": None,
-        }
-        cls.bin = os.getenv("PYTONGUE_TEST_BINARY", "./zig-out/bin/pytongue")
+def test_invalid_request(send_request, read_response):
+    send_request("invalid request")
+    response = read_response()
+    response_parsed = json.loads(response)
+    assert response_parsed["error"] is not None
+    assert response_parsed["id"] is None
 
-        with open("version") as f:
-            cls.version = f.read().strip()
 
-        cls.server = subprocess.Popen(
-            [cls.bin],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            env=os.environ.copy(),
-        )
-        time.sleep(1)
+def test_initialize(send_request, simple_initialize_request, read_response, version):
+    send_request(json.dumps(simple_initialize_request))
+    response = read_response()
+    response_parsed = json.loads(response)
+    assert response_parsed["error"] is None
+    assert response_parsed["id"] == simple_initialize_request["id"]
+    assert response_parsed["result"]["serverInfo"]["version"] == version
 
-    @classmethod
-    def teardown_class(cls):
-        cls.server.terminate()
-        cls.server.wait()
 
-    def send_request(self, request):
-        content_length = len(request)
-        self.server.stdin.write(
-            f"Content-Length: {content_length}\r\n\r\n{request}".encode()
-        )
-        self.server.stdin.flush()
-
-    def read_response(self):
-        header = self.server.stdout.readline().decode().strip()
-        content_length = int(header.split(": ")[1])
-        self.server.stdout.readline()  # Empty line
-        return self.server.stdout.read(content_length).decode()
-
-    def test_invalid_request(self):
-        self.send_request("invalid request")
-        response = self.read_response()
-        response_parsed = json.loads(response)
-        assert response_parsed["error"] is not None
-        assert response_parsed["id"] is None
-
-    def test_initialize(self):
-        self.send_request(json.dumps(self.request_data))
-        response = self.read_response()
-        response_parsed = json.loads(response)
-        assert response_parsed["error"] is None
-        assert response_parsed["id"] == self.request_data["id"]
-        assert response_parsed["result"]["serverInfo"]["version"] == self.version
-
-    def test_invalid_header(self):
-        self.server.stdin.write("invalid header\r\n\r\n".encode())
-        self.server.stdin.flush()
-        response = self.read_response()
-        response_parsed = json.loads(response)
-        assert response_parsed["error"] is not None
-        assert response_parsed["error"]["code"] == -32700
-        assert response_parsed["id"] is None
+def test_invalid_header(send_arbitrary_request, read_response):
+    send_arbitrary_request("invalid header\r\n\r\n")
+    response = read_response()
+    response_parsed = json.loads(response)
+    assert response_parsed["error"] is not None
+    assert response_parsed["error"]["code"] == -32700
+    assert response_parsed["id"] is None
