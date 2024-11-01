6d869129004244ee7826546b5f860e180d2f8726
alex-korzh <alex@korzh.me>
2024-11-01 19:30:36 +0100

    generic filename in tests

--- tests/integration/main.zig
+++ tests/integration/main.zig
@@ -11,15 +11,22 @@ test "parse-python-file" {
     const allocator = testing.allocator;
 
     const p = ts.TreeSitter.ts_parser_new().?;
-    defer ts.TreeSitter.ts_parser_delete(p);
 
     _ = ts.TreeSitter.ts_parser_set_language(p, ts.tree_sitter_python());
+    // FIXME
     var workspace = parser.Workspace.init(
         "/home/alex/Documents/code/zig/pytongue",
         "/home/alex/Documents/code/zig/pytongue/.venv/bin/python",
         p,
         allocator,
     );
-    defer workspace.deinit();
-    _ = try workspace.parseFile("/home/alex/Documents/code/zig/pytongue/tests/assets/main.py", false);
+    const cwd = try std.fs.cwd().realpathAlloc(allocator, ".");
+    const filePath = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ cwd, "tests/assets/main.py" });
+    defer {
+        ts.TreeSitter.ts_parser_delete(p);
+        workspace.deinit();
+        allocator.free(cwd);
+        allocator.free(filePath);
+    }
+    _ = try workspace.parseFile(filePath, false);
 }
